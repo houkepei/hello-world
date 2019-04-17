200de9442e17ca20a73c2324db4e5bd040c3fe37
houkp <17611169761@163.com>
2019-04-17 17:51:28 +0800

    保存黑名单

--- src/main/java/com/houkp/recommended/service/impl/WebServiceImpl.java
+++ src/main/java/com/houkp/recommended/service/impl/WebServiceImpl.java
@@ -1,17 +1,22 @@
 package com.houkp.recommended.service.impl;
 
+import com.houkp.recommended.entity.BlacklistIp;
 import com.houkp.recommended.entity.Order;
+import com.houkp.recommended.repository.BlacklistIpRepository;
 import com.houkp.recommended.repository.OrderRepository;
 import com.houkp.recommended.service.WebService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.*;
 
 
 @Service
@@ -19,6 +24,10 @@ public class WebServiceImpl implements WebService {
 
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private BlacklistIpRepository blacklistIpRepository;
 
     @Override
     public Page<Order> search(Pageable pageable) {
@@ -28,10 +37,36 @@ public class WebServiceImpl implements WebService {
 
     @Override
     public String updateBlacklistIp(Set set, String startDate, String endDate) {
+        LocalDate localDate = LocalDate.now();
+        Date date  =Date.valueOf(localDate);
         List list = new ArrayList(set);
-        System.out.println(list.size());
+        final String strSql = "insert into  blacklist_ip (ip_blacklist, flag, end_time,  start_time, version) values (?, ?, ?, ?, ?)";
+        jdbcTemplate.batchUpdate(strSql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                preparedStatement.setString(1, String.valueOf(list.get(i)));
+                preparedStatement.setInt(2, 1);
+                preparedStatement.setDate(3, date);
+                preparedStatement.setDate(4,  date);
+                preparedStatement.setString(5, startDate);
+            }
 
-        orderRepository.save(list);
-        return null;
+            @Override
+            public int getBatchSize() {
+                return list.size();
+            }
+        });
+//        List<BlacklistIp> list = new ArrayList();
+//        for (Object o : set) {
+//            BlacklistIp blacklistIp = new BlacklistIp();
+//            System.out.println(o.toString());
+//            blacklistIp.setIpBlacklist(o.toString());
+//            blacklistIp.setFlag(1);
+//            list.add(blacklistIp);
+//
+////            blacklistIpRepository.save(blacklistIp);
+//        }
+        System.out.println(list.size());
+        return String.valueOf(list.size());
     }
 }
